1b637dfa4411b60af790132155365bc1f1fa8226
Zsvob <svobodazachary@gmail.com>
2022-02-10 09:51:05 -0500

    setting up tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,29 +2,45 @@ DROP DATABASE IF EXISTS company;
 CREATE DATABASE company;
 USE company;
 
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
+-- DROP TABLE IF EXISTS department;
+-- DROP TABLE IF EXISTS role;
+-- DROP TABLE IF EXISTS employees;
 
 
-CREATE TABLE department (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE department (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL
+-- );
 
 
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(12,2),
-  department_id INTEGER
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
-  manager_id INTEGER,
-  department_id INTEGER
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INTEGER
 );
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 1, 1),
+  ('Piers', 'Gaveston', 1, 0),
+  ('Charles', 'LeRoi', 2, 1),
+  ('Katherine', 'Mansfield', 2, 1),
+  ('Dora', 'Carrington', 3, 0),
+  ('Edward', 'Bellamy', 3, 0),
+  ('Montague', 'Summers', 3, 1),
+  ('Octavia', 'Butler', 3, 1),
+  ('Unica', 'Zurn', 4, 1);
+
+
+SELECT * FROM employees;
+
